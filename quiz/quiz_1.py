04323593a3292e435da4330a8134d58d6027a04a
Shao Hui <shaohui920308@gmail.com>
2017-08-24 11:29:19 +1000

    add quiz_1

---
+++ quiz/quiz_1.py
@@ -0,0 +1,78 @@
+# Written by *** and Eric Martin for COMP9021
+
+
+'''
+Generates a list L of random nonnegative integers smaller than the length of L,
+whose value is input by the user, and outputs two lists:
+- a list M consisting of L's middle element, followed by L's first element,
+  followed by L's last element, followed by L's second element, followed by
+  L's penultimate element...;
+- a list N consisting of L[0], possibly followed by L[L[0]], possibly followed by
+  L[L[L[0]]]..., for as long as L[L[0]], L[L[L[0]]]... are unused, and then,
+  for the least i such that L[i] is unused, followed by L[i], possibly followed by
+  L[L[i]], possibly followed by L[L[L[i]]]..., for as long as L[L[i]], L[L[L[i]]]...
+  are unused, and then, for the least j such that L[j] is unused, followed by L[j],
+  possibly followed by L[L[j]], possibly followed by L[L[L[j]]]..., for as long as
+  L[L[j]], L[L[L[j]]]... are unused... until all members of L have been used up.
+'''
+
+
+import sys
+from random import seed, randrange
+
+
+try:
+    arg_for_seed, length = input('Enter two nonnegative integers: ').split()
+except ValueError:
+    print('Incorrect input, giving up.')
+    sys.exit()
+try:
+    arg_for_seed, length = int(arg_for_seed), int(length)
+    if arg_for_seed < 0 or length < 0:
+        raise ValueError
+except ValueError:
+    print('Incorrect input, giving up.')
+    sys.exit()
+
+seed(arg_for_seed)
+L = [randrange(length) for _ in range(length)]
+print('\nThe generated list L is:')
+print('  ', L)
+M = []
+N = []
+
+index = len(L)//2
+hi = len(L) - 1
+lo = 0
+if len(L)!=0:
+	M.append(L[index])
+while len(M) != len(L):
+	if lo != index:
+		M.append(L[lo])
+		lo += 1
+	if hi != index:
+		M.append(L[hi])
+		hi -= 1
+
+index = 0
+check = [0 for _ in L]
+if len(L)!= 0:
+	N.append(L[0])
+while len(N) != len(L):
+	#print(index)
+	check[index] = 1
+	index = L[index]
+	if check[index] == 1:
+		index = 0
+		while check[index] != 0:
+			index += 1
+	
+	N.append(L[index])
+    
+print('\nHere is M:')
+print('  ', M)
+print('\nHere is N:')
+print('  ', N)
+print('\nHere is L again:')
+print('  ', L)
+
