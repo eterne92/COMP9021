552dce2edae94d3843e46bf1eed722a71657d8d3
eterne92 <shaohui920308@gmail.com>
2017-09-19 16:53:21 +1000

    add some regex to markov

--- lab/lab_8/markov.py
+++ lab/lab_8/markov.py
@@ -2,9 +2,13 @@ from random import choice
 import re
 n = int(input('What n to use to let an n-gram determine the next character? '))
 nwords = int(input('How many words do you want to generate?'))
+a = 0
+b = 0
 with open('dictionary.txt') as f:
-    l = f.read().split('\n')
+    fr = f.read()
+    l = fr.split('\n')
     l = [i + '\n' for i in l]
+    setl = set(l)
     for j in range(nwords):
         word = ''
         i = 0
@@ -14,19 +18,26 @@ with open('dictionary.txt') as f:
                 c = choice(l)[0]
                 word += c
             elif i < n:
-                c = choice([w for w in l if w.startswith(word)])[i]
+                r = re.compile('(?:^|\n)' + word + '(.|\n)')
+                t = choice(re.findall(r,fr))
+                c = t[-1]
                 word += c
             else:
-                t = choice([w for w in l if word[i-n:i+1] in w])
-                c = t[t.find(word[i-n:i+1]) + n]
+                w = word[i-n:i+1]
+                r = re.compile(word[i-n:i+1] + '(.|\n)')
+                t = choice(re.findall(r,fr))
+                c = t[-1]
                 word += c
             i += 1
-        if word in l:
+        if word in setl:
             print('Rediscovered',end = ' ')
+            a += 1
         else:
             print('Invented', end = ' ')
-        print(word[:-1])
+            b += 1
+#        print(word[:-1])
 
+print(a/b)
 
 
         
